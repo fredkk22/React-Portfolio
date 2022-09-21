3b1c11333bbe2d00c11619567393674452a02d0f
fredkk22 <freddykwak123@gmail.com>
2022-09-21 17:22:55 -0400

    Trying to get the images to work

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Footer() {
     return (
         <div>
-            <a href="https://github.com/fredkk22"><img alt="Freddy's Github Profile" src="src\components\images\github-icon.png"></img></a>
-            <a href="https://www.linkedin.com/in/freddy-kwak-a3aa551a6/"><img alt="Freddy's LinkedIn Profile" src="src\components\images\linkedin-icon.png"></img></a>
+            <a href="https://github.com/fredkk22"><img alt="Freddy's Github Profile" src="./src/components/images/github-icon.png"></img></a>
+            <a href="https://www.linkedin.com/in/freddy-kwak-a3aa551a6/"><img alt="Freddy's LinkedIn Profile" src="./src/components/imageslinkedin-icon.png"></img></a>
         </div>
     )
 }
